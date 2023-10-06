1b28ba2299334be7fadbaab8ab32de56be9555b3
Evanrada <marycarmenhr@gmail.com>
2023-10-06 15:56:08 -0600

    home actualizacion

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,50 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:html';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Clientes {
+  final String id;
+  final String nombre;
+  final String edad;
+  final String telefono;
+
+  Clientes({
+    required this.id,
+    required this.nombre,
+    required this.edad,
+    required this.telefono,
+  });
+}
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp().then((_) {
+    runApp(const MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Firestone a clientes',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.pink,
+        ),
+      ),
+      home: const MyHomePage(title: 'Clientes'),
+    );
+  }
+}
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -12,40 +53,204 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void getEval2() async {
-    CollectionReference collectionReference =
-        FirebaseFirestore.instance.collection("tb_test");
-    QuerySnapshot mensajes = await collectionReference.get();
-    if (mensajes.docs.length != 0) {
-      for (var doc in mensajes.docs) {
-        print(doc.data());
-      }
+  final CollectionReference productosCollection =
+      FirebaseFirestore.instance.collection("tb_productos");
+
+  final TextEditingController idController = TextEditingController();
+  final TextEditingController nombreController = TextEditingController();
+  final TextEditingController edadController = TextEditingController();
+  final TextEditingController telefonoController = TextEditingController();
+
+  Future<void> addProducto() async {
+    String id = idController.text.trim();
+    String nombre = nombreController.text.trim();
+    String precio = edadController.text.trim();
+    String stock = telefonoController.text.trim();
+
+    if (id.isNotEmpty &&
+        nombre.isNotEmpty &&
+        edad.isNotEmpty &&
+        telefono.isNotEmpty) {
+      await tb_testCollection.doc(id).set({
+        'nombre': nombre,
+        'precio': precio,
+        'stock': stock,
+      });
+      idController.clear();
+      nombreController.clear();
+      edadController.clear();
+      telefonoController.clear();
+
+      _showSnackbar('Agregado correctamente');
+    } else {
+      _showSnackbar('Por favor, completa todos los campos');
     }
   }
 
-  @override
-  Widget buid(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          children: [Text("Crud a Firebase")],
-        ),
+  Future<List<Clientes>> _getProductos() async {
+    QuerySnapshot Clientes = await tb_testCollection.get();
+    List<Clientes> listaProductos = [];
+    if (Clientes.docs.length != 0) {
+      for (var doc in Clientes.docs) {
+        final data = doc.data() as Map<String, dynamic>;
+        listaProductos.add(Clientes(
+          id: doc.id,
+          nombre: data['nombre'] ?? '',
+          edad: data['edad'] ?? '',
+          telefono: data['telefono'] ?? '',
+        ));
+      }
+    }
+    return listaClientes;
+  }
+
+  Future<void> updateClientes(String id) async {
+    await tb_testCollection.doc(id).update({
+      'nombre': nombreController.text,
+      'precio': edadController.text,
+      'stock': telefonoController.text,
+    });
+
+    // Limpiar los controladores después de actualizar un producto
+    idController.clear();
+    nombreController.clear();
+    edadController.clear();
+    telefonoController.clear();
+
+    _showSnackbar('Datos de los Clientes actualizados correctamente');
+  }
+
+  Future<void> deleteClientes(String id) async {
+    await tb_testCollection.doc(id).delete();
+
+    _showSnackbar('Clientes eliminado correctamente');
+  }
+
+  void _showSnackbar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: Duration(seconds: 1),
       ),
     );
   }
 
-  @override
-  void initState() {
-    super.initState();
-    getEval2();
-  }
+  bool _showUpdateButton = false;
+  Clientes? _selectedClientes;
 
   @override
-  void dispose() {
-    super.dispose();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextFormField(
+              controller: idController,
+              decoration: InputDecoration(labelText: 'ID del Cliente'),
+            ),
+            SizedBox(height: 8.0),
+            TextFormField(
+              controller: nombreController,
+              decoration: InputDecoration(labelText: 'Nombre del Cliente'),
+            ),
+            SizedBox(height: 8.0),
+            TextFormField(
+              controller: edadController,
+              decoration: InputDecoration(labelText: 'Edad del cliente'),
+            ),
+            SizedBox(height: 8.0),
+            TextFormField(
+              controller: telefonoController,
+              decoration: InputDecoration(labelText: 'telefono del cliente'),
+            ),
+            SizedBox(height: 4.0),
+            ElevatedButton(
+              onPressed: () {
+                addClientes();
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red, // Cambiar el color del botón
+                padding: EdgeInsets.symmetric(
+                    horizontal: 2, vertical: 0), // Tamaño personalizado
+              ),
+              child: Text('Agregar'),
+            ),
+            SizedBox(height: 8.0),
+            if (_showUpdateButton)
+              ElevatedButton(
+                onPressed: () {
+                  updateClientes(_selectedClientes!.id);
+                  setState(() {
+                    _showUpdateButton = false;
+                  });
+                },
+                child: Text('Actualizar Clientes'),
+              ),
+            Expanded(
+              child: FutureBuilder<List<Clientes>>(
+                future: _getClientes(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text('Error al cargar los datos'),
+                    );
+                  } else {
+                    List<Clientes>? Clientes = snapshot.data;
+                    return DataTable(
+                      columns: [
+                        DataColumn(label: Text('ID')),
+                        DataColumn(label: Text('Nombre')),
+                        DataColumn(label: Text('Edad')),
+                        DataColumn(label: Text('telefono')),
+                      ],
+                      rows: Clientes!.map((Clientes) {
+                        return DataRow(
+                          cells: [
+                            DataCell(Text('${Clientes.id}')),
+                            DataCell(Text('${Clientes.nombre}')),
+                            DataCell(Text('${Clientes.edad}')),
+                            DataCell(Text('${Clientes.telefono}')),
+                            DataCell(
+                              Row(
+                                children: [
+                                  IconButton(
+                                    icon: Icon(Icons.edit),
+                                    onPressed: () {
+                                      // Puedes implementar la lógica de edición aquí
+                                      print('Editar: ${Clientes.id}');
+                                    },
+                                  ),
+                                  IconButton(
+                                    icon: Icon(Icons.delete),
+                                    onPressed: () {
+                                      // Puedes implementar la lógica de eliminación aquí
+                                      print('Eliminar: ${Clientes.id}');
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        );
+                      }).toList(),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
