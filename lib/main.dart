73f982660fb08a4a1722b1daf92bf94a8760de92
claudymejia <clauditam752@gmail.com>
2023-10-06 15:53:48 -0600

    main
    propio1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 import 'pages/home.dart';
 
 void main() {
@@ -10,18 +11,19 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'CRUD DE CLIENTES',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.red,
+        ),
       ),
-      home: const MyHomePage(title: 'EVALUACION 2'),
+      home: const MyHomePage(title: 'CLIENTES'),
     );
   }
 }
